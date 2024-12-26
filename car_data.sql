c76495e7b2b304428242abda62d6852a28d64799
MrDataProf <olusegunadeolu@hotmail.com>
2024-12-26 15:41:08 +0000

    car_data.sql

---
+++ car_data.sql
@@ -0,0 +1,16 @@
+SELECT * FROM agency.car_data;
+
+select Name, trim(Name) from agency.car_data;
+
+-- SQL Cars Data
+-- QUESTION 1. Read all cars Data
+SELECT * from agency.car_data;
+-- QUESTION 2. Total Cars: To get a count of total records.
+-- QUESTION 3. The Client asked me to show, how many cars will be available in 2023?
+-- QUESTION 4. The Client asked me to show, how many cars will be available in 2020,2021,2022?
+-- QUESTION 5. Client asked me to print the totals of all cars by year. I don't see all the details.
+-- QUESTION 6. Client asked the car dealer agent: how many diesel cars were there in 2020?
+-- QUESTION 7. How many petrol cars were there in 2020?
+-- QUESTION 8. Print the list of all the fuel cars (Petrol,Diesel, and CnG) come by all years?
+-- QUESTION 9. Were more than 100 cars in a given year, which year has more than 100 cars?
+-- QUESTION 10. Count all cars details between 2015 and 2023, need a complete list.
